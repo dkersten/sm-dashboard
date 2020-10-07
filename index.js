983f65b8e22537bd65f94d8b9c78b5cd513141ff
dkersten <dkersten1994@gmail.com>
2020-10-07 14:49:59 -0400

    light/dark theme functionaolity working

--- index.js
+++ index.js
@@ -1,14 +1,29 @@
 const body = document.querySelector ('body');
 const themeCheckbox = document.querySelector ('input[type="checkbox"]');
+const overview = document.querySelector ('section.overview');
+const info = document.querySelector ('section.info');
+const overall = document.querySelector ('section.sm-overall');
 
 themeCheckbox.addEventListener ('change', () => {
   if (themeCheckbox.checked) {
     console.log ('dark mode');
     body.classList.add ('dark');
     body.classList.remove ('light');
+    overview.classList.add ('dark');
+    overview.classList.remove ('light');
+    info.classList.add ('dark');
+    info.classList.remove ('light');
+    overall.classList.add ('dark');
+    overall.classList.remove ('light');
   } else {
     console.log ('light mode');
     body.classList.add ('light');
     body.classList.remove ('dark');
+    overview.classList.add ('light');
+    overview.classList.remove ('dark');
+    info.classList.add ('light');
+    info.classList.remove ('dark');
+    overall.classList.add ('light');
+    overall.classList.remove ('dark');
   }
 });
